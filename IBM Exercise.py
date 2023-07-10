08a54d97eefd659d4b63db8bee9f11be9322c3e0
NiMachado <35191887+NiMachado@users.noreply.github.com>
2023-07-10 15:38:08 -0300

    IBM python file
    Exercise for the course IBM - Tools for Data Science.

---
+++ IBM Exercise.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IBM Exercise
+
+# Introduction: This is an exercise for the course: IBM - Tools for Data Science.
+
+# Many languages can be use for data science but the main languages are Python and R.
+
+# the main data science libraries in Python are:
+# - Pandas (data structure & tools)
+# - NumPy (arrays and matrices)
+# - Matplotlib (plots & graphs)
+# - Seaborn (heatmaps, time series)
+# 
+# High-level machine learning and deep learning libraries in Python:
+# - Scikit-Learn (tools for regression, classification and clustering)
+# - Keras (deep learning neural networks)
+# - TensorFlow (DL) - Low-level framework used in large-scale production of deep learning models. (Production and deployment)
+# - PYTorch (experiments, deep learning regression and classification)
+# 
+# The main data science libraries in R are:
+# - dplyr - Data manipulation
+# - stringr - String manipulation
+# - ggplot - Data visualization
+# - caret - Machine learning
+
+# DATA SCIENCE TOOLS
+# Data management - Sources (collecting and retrieving data)
+# Data integration and transformation - ETL
+# Data visualization - Transforming the values to graphical representation
+# Model building - Train data and analyze patterns
+# Model deployment - Integrating a model into a production environment
+# Model monitoring and assessment - Tools that monitore models
+# 
+# Code asset management - Developers use versioning to track and manage changes to a software project code (GitHub)
+# 
+# Development Environments - IDEs 
+# 
+# OPEN SOURCE TOOLS
+# 
+# | MANAGEMENT (DATABASE) | INTEGRATION (ETL) | VISUALIZATION | DEPLOY MONITOR   | CODE AND MANAGEMENT |
+# | MYSQL                 | APACHE AIRFLOW    | PIXIE DUST    | AP* PREDICTION IO| GIT                 |
+# | MONGO DB              | KUBERFLOW         | HUE           | SELDON           | GITHUB              |
+# | POSTGRES              | APACHE KAFKA      | KIBANA        | KUBERNETES       | GITLAB              |
+# | APACHE COUCH DB       | APACHE NIFI       | AP* SUPERSET  | REDHAT OPENSHIFT | BITBUCKET           |
+# | APACHE CASSANDRA      | APACHE SPARK SQL  |               | MLEAP            | APACHE ATLAS        |
+# | HADOOP                | NODE RED          |               | TENSORFLOW       | ODPI EGEGIA         |
+# | CEPH                  |                   |               | MODELDB          | KYLO                |
+# | ELASTIC SEARCH        |                   |               | PROMETHEUS       |                     |
+# |                       |                   |               | IBM AI FAIRN.360 |                     |
+# |                       |                   |               | IBM ADV. ROB.360 |                     |
+# |                       |                   |               | IBM AI EXPL. 360 |                     |
+# 
+# AP* = APACHE
+# 
+# OPEN SOURCE TOOLS FOR DATA SCIENCE PART 2
+# DEVELOPMENT ENVIRONMENT
+# 
+# - Jupyter Notebooks (most used)
+# - Jupyter Lab
+# - Apache Zeppelin
+# - R Studio
+# - Spyder (mimics R)
+# 
+# Sometimes your data doesn’t fit in a single computer or memory, so you use:
+#     Apache Spark - Batch processing engine - Cluster environment and linear scalability
+#     Apache Flink -Stream processing image - Real time streams
+#     Riselab Ray - Large scale deep learning model training
+# 
+# FULLY INTEGRATED and VISUAL TOOLS
+# 
+# Tools that contain:
+# - Data Integration and Transformation
+# - Data Visualization
+# - Model Building
+# 
+#     Knime - Drag-and-drop capabilities and Built-in visualization
+#     Orange - Built-in visualization
+# 
+# COMMERCIAL TOOLS FOR DATA SCIENCE
+# 
+# | MANAGMENT        | INTEGRATION AND TRANSFORMATION | DATA VISUALIZATION   | MODEL BUILDING            |
+# | ORACLEDB         | INFORMATICA                    | TABLEAU              | SPSS                      |
+# | MICR. SQL SERVER | IBM INFOSPHERE                 | POWER BI             | SAS                       |
+# | IBM DB2          | TALENT                         | IBM COGNOS ANALYTICS | IBM WATSON STUDIO DESKTOP |
+# |                  | IBM WATSON STUDIO DESKTOP      |                      |                           |
+# 
+# SaaS - Software as a Service
+# 
+# FULLY INTEGRATED VISUAL TOOLS AND PLATFORMS
+# Watson Studio
+# Watson Open Scale
+# Azure Machine Learning
+# H20 Driverless AI
+# 
+# DATA MANAGMENT
+# Amazon DynamoDB - NoSQL Database
+# Cloudant
+# CouchDB
+# IBM DB2
+# 
+# DATA INTEGRATION AND TRANSFORMATION - ETL
+# Informatica
+# IBM Data Refinery
+# 
+# DATA VISUALIZATION
+# Datameer
+# IBM Cognos Analytics
+# IBM Watson Studio
+# 
+# MODEL BUILDING
+# IBM Watson Machine Learning
+# Google Cloud
+# 
+# MODEL DEPLOYMENT
+# IBM Watson Studio
+
+# Arithmetic expression examples:
+# 1 + 1 = 2
+# 4 - 2 = 2
+# 1 * 2 = 2
+# 10 / 5 = 2
+
+# In[2]:
+
+
+2 * (2 + 2)
+
+
+# In[4]:
+
+
+180 / 60 
+# minutes to hour
+
+
+# Objectives: 
+# - Use the Jupyter notebook
+# - Use the github enviroment
+# - Make a practical example from the content learned in the course
+
+# Author: Nibian B. A. F. Machado
